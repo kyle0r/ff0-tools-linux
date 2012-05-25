e9ca5f3bffecc270755b965f737945a37e12520e
kyle0r <k@ff0.eu>
2012-05-25 13:44:03 +0200

    created an array for print_stdin() results, reduces duplication of the documentation of the return codes. made /dev/stdin a variable, for future flexiblity and potential handler testing

--- lib/stdin.inc.sh
+++ lib/stdin.inc.sh
@@ -22,7 +22,7 @@
 #
 #    ----------
 #
-#    This program attempts to test shell /dev/stdin and /dev/stdout devices.
+#    This program provides functions related to a shells /dev/stdin device.
 #    Copyright (C) 2012  Kyle       
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -49,46 +49,55 @@
 #    See README file for contact details and more info.
 #    </copyright>
 
+stdin=/dev/stdin
+print_stdin_return[0]="successfully printed ${stdin}"
+print_stdin_return[10]="terminal detected, not interesting, nothing to do"
+print_stdin_return[20]="character device detected, not interesting, nothing to do"
+print_stdin_return[30]="empty file detected, nothing to do"
+print_stdin_return[40]="empty text stream detected, nothing to do"
+print_stdin_return[50]="timeout during read from ${stdin}, probably an empty pipe"
+print_stdin_return[60]="unsuccessful detection of ${stdin}, probably a bug"
+unset stdin
+
 # note: http://stackoverflow.com/questions/6255303/bash-piping-prevents-global-variable-assignment
-# return 0 : successfully printed stdin
-# return 10: terminal, nothing interesting to print
-# return 20: character device, nothing interesting to print
-# return 30: attempting to print empty file
-# return 40: attempting to print empty text stream
-# return 50: unsuccessful read from stdin
-# return 60: unsuccessful detection of /dev/stdin
+# the print_stdin_return array documents return codes
 print_stdin() {
-	local stdinput # local variable to store /dev/stdin in memory
+	local stdin; stdin=/dev/stdin
+	local stdinput # local variable to store $stdin in memory
 	# if dealing with a terminal, nothing to do, return
-	if [[ -t /dev/stdin ]]; then
+	if [[ -t "${stdin}" ]]; then
 		return 10
 	# if dealing with a character device, nothing to do, return
-	elif [[ -c /dev/stdin ]]; then
+	elif [[ -c "${stdin}" ]]; then
 		return 20
-	# if /dev/stdin is a file, we know we are dealing with file input redirection
-	elif [[ -f /dev/stdin ]]; then
-		# use a subshell to store stdin and append the character x
-		# the quotes and echo x preserve whitespace and worksaround bash stripping characters from stdin in the variable assignment
-		stdinput="$(cat </dev/stdin; echo x)"
-		# if stdin is !== character x, we know stdin was not empty
+	# if $stdin is a file, we know we are dealing with file input redirection
+	elif [[ -f "${stdin}" ]]; then
+		# Use a subshell to store $stdin and append the character x
+		# the quotes and echo x preserve whitespace and worksaround
+		# bash stripping characters from $stdin in the variable assignment.
+		stdinput="$(cat <${stdin}; echo x)"
+		# if $stdin is !== character x, we know it was not empty
 		if [[ "$stdinput" != x ]]; then
-			stdinput="${stdinput%x}" # clean up stdin
+			stdinput="${stdinput%x}" # clean up $stdin
 			printf -- "$stdinput"
 			return 0
 		fi
 		return 30
-	# if /dev/stdin is a pipe
-	elif [[ -p /dev/stdin ]]; then
-		# attempt to read the first char from stdin, with a short timeout
-		# use a very unlikely IFS to preserve any IFS characters in the text stream
-		# this approach works when forks and subshells may be a factor
-		# the timeout ensures the read is non-blocking
-		if IFS='\0\0\0\t' read -t0.5 -N1 char </dev/stdin; then
-			# use a subshell to store stdin and append the character x
-			# the quotes and echo x preserve whitespace and worksaround bash stripping characters from stdin in the variable assignment
-			stdinput="${char}$(cat </dev/stdin; echo x)" # variable to store /dev/stdin in memory
+	# if $stdin is a pipe
+	elif [[ -p "${stdin}" ]]; then
+		# Attempt to read the first char from $stdin, with a short timeout
+		# use a very unlikely IFS to preserve any IFS characters in the first
+		# character of $stdin.
+		# This approach works should forks and subshells may be a factor
+		# the timeout ensures the read is non-blocking.
+		# I have tested many alternatives, this approach appears the most robust.
+		if IFS='\0\0\0\t' read -t0.5 -N1 char <"${stdin}"; then
+			# Use a subshell to store $stdin and append the character x
+			# the quotes and echo x preserve whitespace and worksaround 
+			# bash stripping characters from $stdin in the variable assignment.
+			stdinput="${char}$(cat <${stdin}; echo x)" # variable to store "${stdin}" in memory
 			unset char # don't need this
-			# if stdin is !== character x, we know stdin was not empty
+			# if $stdin is !== character x, we know it was not empty
 			if [[ "$stdinput" != x ]]; then
 				stdinput="${stdinput%x}" # clean up stdin
 				printf -- "$stdinput"
