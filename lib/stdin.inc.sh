187c8b18528b06db7ce3482fc7f425b3f30cb752
kyle0r <k@ff0.eu>
2012-05-25 05:44:28 +0200

    Applying Copyright lics

--- lib/stdin.inc.sh
+++ lib/stdin.inc.sh
@@ -1,3 +1,54 @@
+
+#    <copyright license=lgpl>
+#    ff0-tools - a collection of useful linux compatible scripts and programs
+#    Copyright (C) 2012  Kyle       
+#
+#    This file is part of ff0-tools.
+#
+#    ff0-tools is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+# 
+#    ff0-tools is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+# 
+#    You should have received a copy of the GNU General Public License
+#    along with ff0-tools.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    The GNU GPL does NOT permit incorporating ff0-tools into proprietary programs.
+#
+#    ----------
+#
+#    This program attempts to test shell /dev/stdin and /dev/stdout devices.
+#    Copyright (C) 2012  Kyle       
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    ----------
+#
+#    The GNU General Public License is provided in the COPYING file.
+#    The GNU Lesser General Public License is provided in COPYING.LESSER file.
+#    Please note that the LGPL is a set of additional permissions on top of the GPL.
+#
+#    The GNU LGPL DOES permit incorporating this program into proprietary programs.
+#
+#    See README file for contact details and more info.
+#    </copyright>
+
 # note: http://stackoverflow.com/questions/6255303/bash-piping-prevents-global-variable-assignment
 # return 0 : successfully printed stdin
 # return 10: terminal, nothing interesting to print
